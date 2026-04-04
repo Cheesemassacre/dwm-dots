8290b4f16bb0b1ae923fde10877ddb762189eff2
Cheesemassacre <144286404+Cheesemassacre@users.noreply.github.com>
2026-04-04 12:10:41 +0200

    medianot.sh update

--- Scripts/medianot.sh
+++ Scripts/medianot.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-if playerctl -a status 2>/dev/null | grep -q "Playing"; then
-  echo "  $(playerctl -a metadata --format '{{artist}} - {{title}}' | head -n1 | cut -c1-40)"
+
+players=$(playerctl -l 2>/dev/null | grep -v -E 'brave|chromium' | tr '\n' ',' | sed 's/,$//')
+
+if playerctl --player "$players" status 2>/dev/null | grep -q "Playing"; then
+  echo "^c#cc241d^^c#c8c093^ $(playerctl --player "$players" metadata --format '{{title}} - {{artist}}' 2>/dev/null | head -n1 | cut -c1-30) "
 else
-  echo " "
+  echo "^c#727169^ ^c#dcd7ba^"
 fi
