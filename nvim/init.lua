dabdef0825f1bf47d1acd1fea74fa1f6e1457d0e
Cheesemassacre <144286404+Cheesemassacre@users.noreply.github.com>
2026-07-29 14:24:39 +0200

    Updated nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,8 +88,6 @@ require("lazy").setup({
     opts = {},
   },
 
-
-
   {
   "gbprod/cutlass.nvim",
     opts = {
@@ -175,36 +173,63 @@ require("lazy").setup({
   },
 
   {
-    "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-    lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-    dependencies = {
-    -- main one
-    { "ms-jpq/coq_nvim", branch = "coq" },
-
-    -- 9000+ Snippets
-    { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-    -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-    -- Need to **configure separately**
-    { 'ms-jpq/coq.thirdparty', branch = "3p" }
-  },
-
-  init = function()
-    vim.g.coq_settings = {
-        auto_start = true, -- if you want to start COQ at startup
-        auto_start ='shut-up',
-        -- Your COQ settings here
-    }
-  end,
-  config = function()
-    -- Your LSP settings here
-  end,
-  },
-  
-  {
    "nvim-tree/nvim-tree.lua",
      lazy = true,
-  }
+  },
+
+  {
+   'saghen/blink.cmp',
+   -- optional: provides snippets for the snippet source
+   dependencies = { 'rafamadriz/friendly-snippets' },
+
+   -- use a release tag to download pre-built binaries
+   version = '1.*',
+   -- AND/OR build from source
+   -- build = 'cargo build --release',
+   -- If you use nix, you can build from source with:
+   -- build = 'nix run .#build-plugin',
+
+   ---@module 'blink.cmp'
+   ---@type blink.cmp.Config
+   opts = {
+     -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+     -- 'super-tab' for mappings similar to vscode (tab to accept)
+     -- 'enter' for enter to accept
+     -- 'none' for no mappings
+     --
+     -- All presets have the following mappings:
+     -- C-space: Open menu or open docs if already open
+     -- C-n/C-p or Up/Down: Select next/previous item
+     -- C-e: Hide menu
+     -- C-k: Toggle signature help (if signature.enabled = true)
+     --
+     -- See :h blink-cmp-config-keymap for defining your own keymap
+     keymap = { preset = 'default' },
+
+     appearance = {
+       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+       -- Adjusts spacing to ensure icons are aligned
+       nerd_font_variant = 'mono'
+     },
+
+     -- (Default) Only show the documentation popup when manually triggered
+     completion = { documentation = { auto_show = false } },
+
+     -- Default list of enabled providers defined so that you can extend it
+     -- elsewhere in your config, without redefining it, due to `opts_extend`
+     sources = {
+       default = { 'lsp', 'path', 'snippets', 'buffer' },
+     },
+
+     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+     -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+     --
+     -- See the fuzzy documentation for more information
+     fuzzy = { implementation = "prefer_rust_with_warning" }
+   },
+   opts_extend = { "sources.default" }
+}
 
 })
 
